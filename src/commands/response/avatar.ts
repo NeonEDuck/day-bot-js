a8a8bf1b67f6beb5b2ec65c53017b2ed30b076bb
NEDuck <cj96cj9696cj@gmail.com>
2024-04-07 03:47:42 +0800

    Change avatar command split `/avatar set` into `/avatar set link` and `/avatar set file` with subcommand group.

--- src/commands/response/avatar.ts
+++ src/commands/response/avatar.ts
@@ -1,63 +1,64 @@
-import { ActionRowBuilder, EmbedBuilder, SlashCommandBuilder, StringSelectMenuBuilder } from 'discord.js'
+import { SlashCommandBuilder } from 'discord.js'
 import { Command } from '../../typing/commands.ts'
-import { query, transaction } from '../../db.ts'
-import { combination, groupBy, logObject, zip } from '../../utils/functions.ts'
-import { ValidationError } from '../../utils/classes.ts'
-
-
-const makePollEmbed = (data: any) => {
-    const embed = new EmbedBuilder()
-        .setTitle(`「${data.title}」`)
-        .setAuthor({name: '投票'})
-        .addFields({name: 'example', value: '3', inline: false})
-        .setFooter({text: `${'≡'.repeat(43)}\n點擊下面選單以投票`})
-    const component = new ActionRowBuilder()
-        .addComponents(
-            new StringSelectMenuBuilder()
-                .addOptions({label: 'example 1', value: '0'}, {label: 'example 2', value: '1'}, {label: 'example 3', value: '2'})
-                .setCustomId('')
-        )
-    return {embed, component}
-}
 
 export default new Command({
     data: new SlashCommandBuilder()
         .setName('avatar')
         .setDescription('?')
-        .addSubcommand(sub =>
-            sub.setName('set')
+        .addSubcommandGroup(grp =>
+            grp.setName('set')
                 .setDescription('Set bot\'s avatar.')
                 .setDescriptionLocalization('zh-TW', '設置機器人的大頭貼')
-                .addStringOption(option =>
-                    option.setName('link')
-                        .setNameLocalization('zh-TW', '連結')
-                        .setDescription('Link of the image.')
-                        .setDescriptionLocalization('zh-TW', '圖片連結。')
-                        .setRequired(false))
-                .addAttachmentOption(option =>
-                    option.setName('file')
-                        .setNameLocalization('zh-TW', '檔案')
-                        .setDescription('The image file.')
-                        .setDescriptionLocalization('zh-TW', '圖片檔案。')
-                        .setRequired(false))
+                .addSubcommand(sub =>
+                    sub.setName('link')
+                        .setDescription('Set bot\'s avatar via link.')
+                        .setDescriptionLocalization('zh-TW', '使用連結設置機器人的大頭貼')
+                        .addStringOption(option =>
+                            option.setName('link')
+                                .setNameLocalization('zh-TW', '連結')
+                                .setDescription('Link of the image.')
+                                .setDescriptionLocalization('zh-TW', '圖片連結。')
+                                .setRequired(true))
+                )
+                .addSubcommand(sub =>
+                    sub.setName('file')
+                        .setDescription('Set bot\'s avatar via file.')
+                        .setDescriptionLocalization('zh-TW', '使用檔案設置機器人的大頭貼')
+                        .addAttachmentOption(option =>
+                            option.setName('file')
+                                .setNameLocalization('zh-TW', '檔案')
+                                .setDescription('The image file.')
+                                .setDescriptionLocalization('zh-TW', '圖片檔案。')
+                                .setRequired(true))
+                ),
         ),
     run: async (ctx, client) => {
-        const subcommand = ctx.options.getSubcommand()
+        const subcommand = `${ctx.options.getSubcommandGroup()} ${ctx.options.getSubcommand()}`
         const subfunctions: {[key: string]: () => Promise<void>} = {
-            'set': async () => {
-                const link      = ctx.options.getString('link')
-                const file      = ctx.options.getAttachment('file')
+            'set link': async () => {
+                const link = ctx.options.getString('link')
                 await ctx.deferReply({ephemeral: true})
 
-                if (link == null && file == null) {
+                if (link == null) {
                     await ctx.editReply({content: 'You must provide at least one option!'});
                     return
                 }
-
-                await client.user?.setAvatar(file?.url ?? link)
+                await client.user?.setAvatar(link)
 
                 await ctx.editReply({content: 'Avatar set!'});
-            }
+            },
+            'set file': async () => {
+                const file = ctx.options.getAttachment('file')
+                await ctx.deferReply({ephemeral: true})
+
+                if (file == null) {
+                    await ctx.editReply({content: 'You must provide at least one option!'});
+                    return
+                }
+                await client.user?.setAvatar(file?.url)
+
+                await ctx.editReply({content: 'Avatar set!'});
+            },
         }
         subcommand in subfunctions && await subfunctions[subcommand]()
     }
